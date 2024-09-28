a160c5ee98b67718ad24ecb9e3d87f558652ebcb
Perewz Alam <theperwezalam2002@gmail.com>
2024-09-28 18:58:23 +0530

    pushed commit

---
+++ frontend/src/environment.js
@@ -0,0 +1,9 @@
+let IS_PROD = true;
+// const server = IS_PROD ?
+//     "https://cyberbackend.onrender.com" :
+
+//     "http://localhost:8000"
+const server="http://localhost:8000"
+
+
+export default server;
